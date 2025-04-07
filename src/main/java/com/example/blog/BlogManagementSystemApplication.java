517b7c8b5496592bca6650956d9566ed0c6eb098
Greta Kastrati <kastratigreta1@gmail.com>
2025-04-07 19:24:01 +0200

    Initialize first project structure

---
+++ src/main/java/com/example/blog/BlogManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogManagementSystemApplication.class, args);
+		System.out.println();
+		System.out.println("Hello world");
+	}
+
+}
